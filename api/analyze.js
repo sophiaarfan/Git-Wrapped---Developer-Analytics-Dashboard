3009c3326b366c01c6207cfad89665ab799864c8
Sophia Arfan <arfans1@mcmaster.ca>
2026-03-07 20:55:26 -0500

    change ai model

--- api/analyze.js
+++ api/analyze.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         }`;
 
         const AIresponse = await fetch(
-           `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+           `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`,
             {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
